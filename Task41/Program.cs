fa5f1049a4b75463ca3e0d57749b8e8e765cd8dd
Nataliya <arnicaaliya@gmail.com>
2023-01-25 18:53:57 +0300

    Подсчет чисел больше нуля в массиве

---
+++ Task41/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел в одну линию 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите элементы(через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = arr.Count(x => x > 0);
+Console.WriteLine($"Кол-во элементов > 0: {count}");
